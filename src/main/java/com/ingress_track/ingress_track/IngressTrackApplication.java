a384e9c99b63feb68fe5c6246acdd5e75fd99857
ALPHA\USER <vallesmark15@gmail.com>
2025-07-07 11:34:19 +0800

    add spring boot init

---
+++ src/main/java/com/ingress_track/ingress_track/IngressTrackApplication.java
@@ -0,0 +1,13 @@
+package com.ingress_track.ingress_track;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IngressTrackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IngressTrackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ingress_track/ingress_track/IngressTrackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ingress_track.ingress_track;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IngressTrackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
